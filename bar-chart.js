6a10716ddf7abae3aafc951900cbd411f1554855
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-06-04 23:41:55 +0000

    updated html file to include required customization options

---
+++ bar-chart.js
@@ -0,0 +1,10 @@
+
+
+// drawBarChart(data, options, element);
+// The data parameter will be the data the chart should work from Start with just an Array of numbers
+// e.g. [1, 2, 3, 4, 5]
+
+// The options parameter should be an object which has options for the chart.
+// e.g. width and height of the bar chart
+
+// The element parameter should be a DOM element or jQuery element that the chart will get rendered into.
