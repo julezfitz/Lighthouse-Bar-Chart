245e20c34f7302001db6938bbdb610062d4906da
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-06-06 15:18:22 +0000

    created mockup bar chart in css/html and added jquery to pull title and append to chart

--- bar-chart.js
+++ bar-chart.js
@@ -1,8 +1,17 @@
+
 $(document).ready(function(){
   $("button").click(function(){
     $(".popup").hide();
+    let chartTitle = $("#title").val();
+
+    $('<div/>',{
+      text: chartTitle,
+  }).appendTo('.title');
+
+    $('.barchart').removeClass('hidden');
   });
 });
+
 
 // drawBarChart(data, options, element);
 // The data parameter will be the data the chart should work from Start with just an Array of numbers
